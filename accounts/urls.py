9445533a89e4783fa16557bc2723ee5f9a592eb9
eliaszpiotr <eliasz.piotr@icloud.com>
2025-11-26 18:58:42 +0100

    templates for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,10 @@
+# accounts/urls.py
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path("login/", views.login_view, name="login"),
+    path("register/", views.register_view, name="register"),
+    path("profile/", views.profile_view, name="profile"),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_view(request):
+    return render(request, "accounts/login.html")
+
+def register_view(request):
+    return render(request, "accounts/register.html")
+
+def profile_view(request):
+    return render(request, "accounts/profile.html")
