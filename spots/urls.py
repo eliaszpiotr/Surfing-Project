a0b0ffcbb62cf2a853467a68b3f29314ffb40064
eliaszpiotr <eliasz.piotr@icloud.com>
2025-12-02 15:39:50 +0100

    spots list and detail view

--- spots/urls.py
+++ spots/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import SpotCreateView
+from .views import SpotListView, SpotDetailView, SpotCreateView, SpotMapDataView
+
+app_name = "spots"
 
 urlpatterns = [
-    # path('', SpotListView.as_view(), name='spot_list'),
-    path('new/', SpotCreateView.as_view(), name='spot_create'),
-    # path('<slug:slug>/', SpotDetailView.as_view(), name='spot_detail'),
+    path("", SpotListView.as_view(), name="spot_list"),
+    path("new/", SpotCreateView.as_view(), name="spot_create"),
+    path("map-data/", SpotMapDataView.as_view(), name="spot_map_data"),
+    path("<slug:slug>/", SpotDetailView.as_view(), name="spot_detail"),
 ]
--- spots/views.py
+++ spots/views.py
@@ -1,19 +1,35 @@
-from django.views.generic import CreateView
+from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.utils.text import slugify
 import uuid
+from django.views import View
+from django.http import JsonResponse
 
 from .models import Spot
 from .forms import SpotForm
+
+
+class SpotListView(ListView):
+    model = Spot
+    template_name = "spots/spot_list.html"
+    context_object_name = "spots"
+
+
+class SpotDetailView(DetailView):
+    model = Spot
+    template_name = "spots/spot_detail.html"
+    context_object_name = "spot"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
 
 
 class SpotCreateView(LoginRequiredMixin, CreateView):
     model = Spot
     form_class = SpotForm
     template_name = "spots/spot_form.html"
-    success_url = reverse_lazy("home")
+    success_url = reverse_lazy("spots:spot_list")
 
     login_url = "login"
     redirect_field_name = "next"
@@ -28,3 +44,26 @@ class SpotCreateView(LoginRequiredMixin, CreateView):
         spot.save()
         messages.success(self.request, f"Surf spot '{spot.name}' has been added successfully!")
         return super().form_valid(form)
+
+
+class SpotMapDataView(View):
+    def get(self, request):
+        spots = (
+            Spot.objects
+            .exclude(latitude__isnull=True)
+            .exclude(longitude__isnull=True)
+        )
+
+        data = [
+            {
+                "name": spot.name,
+                "slug": spot.slug,
+                "lat": float(spot.latitude),
+                "lng": float(spot.longitude),
+                "difficulty": spot.get_difficulty_display(),
+                "country": str(spot.country),
+            }
+            for spot in spots
+        ]
+
+        return JsonResponse(data, safe=False)
